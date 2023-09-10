932c09e32fe6a34d819454cae0edb9fbc83b02e9
Alex <86182397+alehxs@users.noreply.github.com>
2023-09-10 13:39:34 -0500

    Takes inputs

--- openwebsite.py
+++ openwebsite.py
@@ -4,12 +4,17 @@ import time
 
 print("Running openwebsite.py\n")
 # Set the URL of the website you want to open
-url = ''
+url = input("Enter url:")
 
 
 print ("Time right now: " + str(datetime.datetime.now().time()))
+
 # Set the time at which you want to open the website
-open_time = datetime.time(13, 45, 00)  # Open at 9:00:00 AM
+# open_time = datetime.time(11, 1, 00)  # Open at 9:00:00 AM
+user_input = input("Enter time to open website (HH MM):")
+hour, minute = user_input.split(" ")
+open_time = datetime.time(int(hour), int(minute), 00)
+
 print("Website will run @ " + str(open_time) + "\n")
 
 
@@ -18,7 +23,7 @@ while True:
     now = datetime.datetime.now().time()
     if now >= open_time:
         break
-    time.sleep(1)
+    time.sleep(10)
 
 # Open the website in the default web browser
 webbrowser.open(url)
